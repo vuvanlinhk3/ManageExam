af11221682403fdff2830d2848a259cf57c0bb1c
vanlinhk3 <vanlinh30122003@gmail.com>
2024-03-04 22:25:28 +0700

    a

---
+++ ManageExam/admin/view/diaLog/selectAddExam.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ManageExam.admin.view.diaLog
+{
+    /// <summary>
+    /// Interaction logic for selectAddExam.xaml
+    /// </summary>
+    public partial class selectAddExam : Window
+    {
+        public selectAddExam()
+        {
+            InitializeComponent();
+        }
+    }
+}
