794bdf5102df54e8a1c5bd53afb5523a44d9c8e4
QQuanNA <garvisgenial@gmail.com>
2024-03-23 14:49:12 +0700

    upppppp

--- ManageExam/admin/view/studentResult/studentResult.xaml.cs
+++ ManageExam/admin/view/studentResult/studentResult.xaml.cs
@@ -11,17 +11,46 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
-namespace ManageExam.admin.view.studentResult
+namespace ManageExam.admin.view
 {
     /// <summary>
     /// Interaction logic for studentResult.xaml
     /// </summary>
     public partial class studentResult : Window
     {
+        string connectstring = @"";
+        SqlConnection connection;
+        SqlCommand cmd;
+        SqlDataAdapter adt;
+        DataTable dt = new DataTable();
         public studentResult()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            connection = new SqlConnection(connectstring);
+        }
+
+        private void Result_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                cmd = new SqlCommand("select * from ketqua", connection);
+                adt = new SqlDataAdapter(cmd);
+                adt.Fill(dt);
+                datagrid.ItemsSource = dt.DefaultView;
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
